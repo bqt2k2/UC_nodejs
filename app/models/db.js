3f253f4c66b9464c3de8467ec6b86f3fd96c9817
bqt2k2 <126138299+bqt2k2@users.noreply.github.com>
2025-04-12 19:36:31 +0700

    Update db.js

--- app/models/db.js
+++ app/models/db.js
@@ -5,7 +5,8 @@ const connection = mysql.createConnection({
     host: dbConfig.HOST,
     user: dbConfig.USER,
     password: dbConfig.PASSWORD,
-    database: dbConfig.DB
+    database: dbConfig.DB,
+    connectTimeout: 60000 // Thêm timeout 30 giây
 });
 
 connection.connect(error => {
